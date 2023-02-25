ace40ab2c5746ca77fce65d797cb334a0b7da5a9
Jasmeet Sharma <jasmeetsharma93@gmail.com>
2023-02-24 23:32:41 -0500

    Conditional checks

--- src/Components/TeamLanding.js
+++ src/Components/TeamLanding.js
@@ -5,14 +5,14 @@ import GameCard from './GameCard'
 export default function TeamLanding(props) {
     const { teams, tid } = props
 
-    let team = teams.filter((tt) => tt.id === tid)
+    let team = teams.filter((tt) => tt.id+'' === tid)
     const [games] = useContext(GamesContext)
     let nextGame, lastGame
-    nextGame = games.filter((g) => {
-        return ((g.hteamid === team[0].id || g.ateamid === team[0].id) && (g.complete !== 100))
+    nextGame = games?.filter((g) => {
+        return ((g.hteamid === team[0]?.id || g.ateamid === team[0]?.id) && (g.complete !== 100))
     })[0]
-    lastGame = games.filter((g) => {
-        return ((g.hteamid === team[0].id || g.ateamid === team[0].id) && (g.complete === 100))
+    lastGame = games?.filter((g) => {
+        return ((g.hteamid === team[0]?.id || g.ateamid === team[0]?.id) && (g.complete === 100))
     })
     lastGame = lastGame[lastGame.length - 1]
     return (
@@ -20,23 +20,24 @@ export default function TeamLanding(props) {
             <section className='team-landing'>
                 <div className="hero-image">
                     <div className="hero-text">
-                        <h1>{team[0].name}</h1>
+                        <h1>{team[0]?.name}</h1>
                         <div className="team-logo-banner">
-                            <img src={`https://squiggle.com.au/${team[0].logo}`} className="banner-logo" title="team logo" alt="logo"/>
+                            <img src={`https://squiggle.com.au/${team[0]?.logo}`} className="banner-logo" title="team logo" alt="logo"/>
                         </div>
                     </div>
                 </div>
             </section>
             <div className='container' style={{ marginTop: '50px' }}>
                 <section className='team-games-section'>
-                    <h2 className="display-3">Next Match</h2>
-                    {nextGame && <GameCard game={nextGame} />}
-                    <h2 className="display-3">Last Match</h2>
-                    {lastGame && <GameCard game={lastGame} />}
+                    {nextGame && <>
+                        <h2 className="display-3">Next Match</h2>
+                        <GameCard game={nextGame} />
+                    </>}
+                    {lastGame && <>
+                        <h2 className="display-3">Last Match</h2>
+                        <GameCard game={lastGame} />
+                    </>}
                 </section>
-                {/* <aside>
-                    <Standings tPage='true'></Standings>
-                </aside> */}
             </div>
         </div>
     )
